1ebdd85954af7c46def566d61be9fef723deade6
danielrkling <danielrkling@gmail.com>
2025-07-01 08:45:22 -0400

    0.0.3

--- extension.js
+++ extension.js
@@ -1,22 +1,32 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+const vscode = require('vscode')
+const fs = vscode.workspace.fs
+
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
+
+/**
+ * 
+ * @param {vscode.ExtensionContext} context 
+ */
 export function activate(context) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "helloworld-sample" is now active!');
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand('helloworld.helloWorld', () => {
+  let disposable = vscode.commands.registerCommand('vscode-web-rolldown.bundle', () => {
     // The code you place here will be executed every time your command is executed
 
-    // Display a message box to the user
-    vscode.window.showInformationMessage('Hello World!');
+
+
+    fs.readFile("rolldown.config.js").then(results=> {
+      vscode.window.showInformationMessage(results)
+      fs.writeFile("main.js","console.log(1)")
+    
+    })
+
   });
 
   context.subscriptions.push(disposable);
